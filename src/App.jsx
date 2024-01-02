93487d733af0f015296192d5523b93646106504c
Ruslan0019 <133685041+Ruslan0019@users.noreply.github.com>
2024-01-02 17:13:04 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,6 @@ import { Section } from 'components/Section/Section';
 import { Notification } from 'components/Notification/Notification';
 
 export const App = () => {
-  // state = {
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  //   total: 0,
-  //   feedback: 0,
-  // };
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -62,87 +55,3 @@ export const App = () => {
     </>
   );
 };
-// handleClick = () => {
-//   this.setState(
-//     prevState => ({
-//       good: prevState.good + 1,
-//     }),
-//     this.updateFeedbackStats
-//   );
-// };
-// const handleClick = () => {
-//   setGood(prevGood => prevGood + 1)
-// }
-
-// handleClick1 = () => {
-//   this.setState(
-//     prevState => ({
-//       neutral: prevState.neutral + 1,
-//     }),
-//     this.updateFeedbackStats
-//   );
-// };
-// const handleClick1 = () => {
-//   setNeutral(prevneutral => prevneutral + 1)
-// }
-
-// handleClick2 = () => {
-//   this.setState(
-//     prevState => ({
-//       bad: prevState.bad + 1,
-//     }),
-//     this.updateFeedbackStats
-//   );
-// };
-// const handleClick2 = () => {
-//   setBad(prevbad => prevbad + 1)
-// }
-
-// countTotalFeedback = () => {
-//   const { good, neutral, bad } = this.state;
-//   return good + neutral + bad;
-// };
-
-// countPositiveFeedbackPercentage = () => {
-//   const { good } = this.state;
-//   const total = this.countTotalFeedback();
-//   return total === 0 ? 0 : Math.floor((good / total) * 100);
-// };
-
-// updateFeedbackStats = () => {
-//   const total = this.countTotalFeedback();
-//   const feedbackPercentage = this.countPositiveFeedbackPercentage();
-
-//   this.setState({
-//     total,
-//     feedback: feedbackPercentage.toFixed(2),
-//   });
-// };
-
-// render() {
-//   const { good, neutral, bad, total, feedback } = this.state;
-
-// return (
-//   <>
-//     <Section title="Please leave feedback">
-//       <FeedbackOptions
-//         handleClick={this.handleClick}
-//         handleClick1={this.handleClick1}
-//         handleClick2={this.handleClick2}
-//       />
-//     </Section>
-//     <Section title="Statistics">
-//       {total > 0 ? (
-//         <Statistics
-//           good={good}
-//           neutral={neutral}
-//           bad={bad}
-//           total={total}
-//           feedback={feedback}
-//         />
-//       ) : (
-//         <Notification message="There is no feedback" />
-//       )}
-//     </Section>
-//   </>
-// );
